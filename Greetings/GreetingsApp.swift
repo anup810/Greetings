04d20303907620c814117fe77f1c0dec42bf1ce3
Anup Saud <anupsaud10@gmail.com>
2025-02-11 22:14:31 -0500

    add project folder

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Anup Saud on 2025-02-11.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
