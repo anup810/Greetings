857a83c1172f56a9021bb3ffc6e3489b2f269351
Anup Saud <anupsaud10@gmail.com>
2025-02-19 10:39:12 -0500

    Implement TipKit and Refactor ContextMenu to Menu

---
+++ Greetings/Tip/GreetingsTip.swift
@@ -0,0 +1,8 @@
+//
+//  GreetingsTip.swift
+//  Greetings
+//
+//  Created by Anup Saud on 2025-02-19.
+//
+
+import Foundation
---
+++ Greetings/Views/LanguageOptionsTapView.swift
@@ -0,0 +1,18 @@
+//
+//  LanguageOptionsTapView.swift
+//  Greetings
+//
+//  Created by Anup Saud on 2025-02-19.
+//
+
+import SwiftUI
+
+struct LanguageOptionsTapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LanguageOptionsTapView()
+}
